173dbfaca5017b48625fa2d22695f550756e51a4
define <govind.birajdar@definelabs.com>
2023-10-01 15:32:58 +0530

    Make app production ready

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
     def hello
         render html: 'Heelo world'
     end
+
+    def home
+      
+    end
+  
+    def about
+      
+    end
+  
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,10 @@
 class PagesController < ApplicationController
     def home
-      render html: 'hello'
+      
     end
   
     def about
-        render html: 'This is about page'
-        
+      
     end
   end
   
